d1a37b3d04eba33586b4283e4b37ab1463967dce
Dayeon Yoon <yday1223@gmail.com>
2024-01-20 10:43:42 +0900

    feat: fibonacci-sequence

---
+++ math-algorithm/fibonacci-sequence.js
@@ -0,0 +1,14 @@
+function fibonacci(n) {
+  const resultLength = n - 2;
+  let result = [0, 1];
+
+  for (let i = 0; i < resultLength; i++) {
+    const firstPreviousNum = result[result.length - 2];
+    const secondPreviousNum = result[result.length - 1];
+    result.push(firstPreviousNum + secondPreviousNum);
+  }
+  console.log(result);
+  return result;
+}
+
+fibonacci(7);
