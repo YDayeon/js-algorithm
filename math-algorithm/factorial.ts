4957a7b03c4e62085f40f8fabd2d2a5fb94d97e6
Dayeon Yoon <yday1223@gmail.com>
2024-01-20 10:57:57 +0900

    factorial algorithm

---
+++ math-algorithm/factorial.ts
@@ -0,0 +1,13 @@
+// Big-O = O(n)
+function factorial(num) {
+  let result = 1;
+
+  if (num === 0 || num === 1) return result;
+
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorial(4));
