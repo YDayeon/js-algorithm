b010c077fc8895002ada91916f63ec56b5ce3e6e
Dayeon Yoon <yday1223@gmail.com>
2024-01-27 14:47:28 +0900

    refactor the 'prime-number' answer

--- math-algorithm/prime-number.js
+++ math-algorithm/prime-number.js
@@ -1,11 +1,12 @@
 const isPrime = (number) => {
-  if (number % 2 === 0) return false;
+  if (number < 2) return false;
 
-  for (let i = 3; i <= number / 2; i++) {
+  for (let i = 2; i <= number / 2; i++) {
     if (number % i === 0) return false;
   }
   return true;
 };
 
+console.log(isPrime(1));
 console.log(isPrime(5));
 console.log(isPrime(4));
